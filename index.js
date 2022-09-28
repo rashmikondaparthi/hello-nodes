6d09a0e7fb47263b655c68acee461aaf0bfdf89a
rashmikondaparthi <100505034+rashmikondaparthi@users.noreply.github.com>
2022-09-28 18:45:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 9014;                 //Save the port number where your server will be listening
-
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
